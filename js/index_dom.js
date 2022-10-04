bdf1825f8a1606f7a6e0fbd0b4b8f8e2f465869b
NATALIA HILARION <77810734+NataliaHilarion@users.noreply.github.com>
2022-10-03 21:17:13 -0500

    Add files via upload

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,11 @@
 import hamburgerMenu from "./menu_hamburger.js";
+import {digitalClock, alarm} from "./sectionOneClock.js"
 
 const d = document
 
 d.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a")
+  digitalClock("#clock", "#startClock", "#stopClock")
+  alarm("assets/alarm-clock.mp3", "#startAlarm", "#stopAlarm")
 })
+
